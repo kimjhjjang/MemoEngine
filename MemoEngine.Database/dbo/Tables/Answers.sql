555f8ed206e0e4fc3f22eb28027e64fb73db40c3
정훈 김 <kimjhjjang@gmail.com>
2021-03-31 21:12:16 +0900

    answer Table, answer Comment Table Create , DAO Create

---
+++ MemoEngine.Database/dbo/Tables/Answers.sql
@@ -0,0 +1,48 @@
+﻿-- 게시판용 테이블 설계
+
+CREATE TABLE [dbo].[Answers]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1,1),
+
+	--5W1H
+	Name NVarchar(25) Not null,
+	PostDate DateTime Default GetDate(),
+	PostIp NVarchar(15) null,
+	Title NVarchar(150) Not null,
+	Content Nvarchar(Max) Not null,
+	Category NVarchar(20) Default('free') null,
+
+
+	--게시판 주요 컬럼
+	Email Nvarchar(100) null,
+	Password Nvarchar(255) null,
+	ReadCount Int Default 0,
+	Encoding Nvarchar(20) null,
+	Homepage Nvarchar(100) null,
+	ModifyDate DateTime null,
+	ModifyIp NVarchar(15) null,
+	CommentCount Int Default 0,
+
+	-- 자료실 게시판 관련 주요 컬럼
+	FileName NVarchar(255) null,
+	FileSize Int Default 0,
+	DownCount Int Default 0,
+
+	-- 답변형 게시판 관련 주요 컬럼
+	Ref int Not null,
+	Step int Default 0,
+	RefOrder  int Default 0,
+	AnswerNum  int Default 0,
+	ParentNum  int Default 0,
+
+	-- 기타 확장 기능 관련 주요 컬럼
+	Num Int Null,
+	UID Int Null,
+	UserId Int Null,
+	UserName NVarchar(25) null,
+	DvisionId Int Default 0 null,
+	CategoryId Int null default 0,
+	BoardId Int Null Default 0,
+	AplicationId Int Null Default 0
+)
+Go
---
+++ MemoEngine.Database/dbo/Tables/Answers_Comments.sql
@@ -0,0 +1,27 @@
+﻿-- 댓글 테이블 생성
+CREATE TABLE [dbo].[Answers_Comments]
+(
+ Id Int Identity(1,1) Not null Primary key,
+ ArticleId Int Not null,
+
+ -- 5w1H
+ Name NVarchar(25) not null,
+ PostDate DateTime Default GetDate(),
+ PostIp NVarchar(15) null,
+ Title Nvarchar(150) null,
+ Content Nvarchar(Max) Not null,
+ Category Nvarchar(20) Default('free') null,
+
+ --[1] 댓글 관련 주요 컬럼 : 레거시
+ Opinion Nvarchar(4000) null,
+ BoardName Nvarchar(50) null,
+ Password Nvarchar(255) null,
+
+ -- 기타 확장 기능 관련 주요 컬럼
+	Num Int Null,
+	UserId Int Null,
+	CategoryId Int null default 0,
+	BoardId Int Null Default 0,
+	AplicationId Int Null Default 0
+)
+go
