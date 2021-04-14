3d2e9feee8045874a34cdf5aa8e369e881f3b93e
정훈 김 <kimjhjjang@gmail.com>
2021-04-14 23:49:46 +0900

    게시판 리스트 페이지에서 사용되는 GetAll 등 API 구현

--- MemoEngine.Answers/AnswerRepository.cs
+++ MemoEngine.Answers/AnswerRepository.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Linq;
 
 namespace MemoEngine.Answers
 {
     public class AnswerRepository : IAnswerRepository
     {
+        private readonly SqlConnection db;
+
+        public AnswerRepository(string connectionString)
+        {
+            db = new SqlConnection(connectionString);
+        }
+
         public Answer Add(Answer model)
         {
             throw new NotImplementedException();
@@ -30,6 +40,23 @@ namespace MemoEngine.Answers
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// GetAll(); FindAll() 형태로 주로 사용 또는 GetNotes(), GetComments(), GetAnswers() 형태로 많이 사용
+        ///</summary>
+        public List<Answer> GetAll(int pageNumber, int pageSize = 10)
+        {
+            try 
+            {
+                var parameters = new DynamicParameters(new { PageNumber = pageNumber, PageSize = pageSize });
+                return db.Query<Answer>("AnswersList",
+                                        parameters, commandType : 
+                                        CommandType.StoredProcedure).ToList();
+            }
+            catch(System.Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
 
         public List<Answer> GetAnswers(int pagenumber, int pagesize = 10)
         {
@@ -41,14 +68,35 @@ namespace MemoEngine.Answers
             throw new NotImplementedException();
         }
 
+        //전체 레코드수 반환 : Answers 테이블의 모든 레코드 수
         public int GetCountAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return db.Query<int>("Select Count(*) From Answers").SingleOrDefault();
+            }catch(System.Exception)
+            {
+                return -1;
+            }
         }
 
+        //검색 결과에 따른 총 레코드 카운트
         public int GetCountBySearch(string searchField, string searchQuery)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return db.Query<int>("AnswersSearchCount", new
+                {
+                    SearchField = searchField,
+                    SearchQuery = searchQuery
+                },
+                    commandType: CommandType.StoredProcedure)
+                    .SingleOrDefault();
+            }
+            catch(System.Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
         }
 
         public string GetFileNameById(int id)
@@ -68,7 +116,9 @@ namespace MemoEngine.Answers
 
         public List<Answer> GetSearchAll(string searchField, string searchQuery, int pageNumber, int pageSize = 10)
         {
-            throw new NotImplementedException();
+            var parameters = new DynamicParameters(
+                new { SearchField = searchField, SearchQuery = searchQuery, PageNumber = pageNumber, PageSize = pageSize });
+            return db.Query<Answer>("AnswersSearchList", parameters, commandType: CommandType.StoredProcedure).ToList();
         }
 
         public List<Answer> GetSummaryByCategory(string category)
--- MemoEngine.Answers/IAnswerRepository.cs
+++ MemoEngine.Answers/IAnswerRepository.cs
@@ -9,10 +9,20 @@ namespace MemoEngine.Answers
         int delete(int id, string password);
         
         List<Answer> GetAnswers(int pagenumber, int pagesize = 10);
-
+        /// <summary>
+        ///전체 레코드 수 반환 : Answers 테이블의 모든 레코드 수
+        ///</summary>
         int GetCountAll();
 
+        /// <summary>
+        ///검색 결과에 따른 총 레코드 카운트
+        ///</summary>
         int GetCountBySearch(String searchField, String searchQuery);
+
+        /// <summary>
+        /// GetAll(); FindAll() 형태로 주로 사용 또는 GetNotes(), GetComments(), GetAnswers() 형태로 많이 사용
+        ///</summary>
+        List<Answer> GetAll(int pageNumber, int pageSize = 10);
 
         List<Answer> GetSearchAll( String searchField, String searchQuery, int pageNumber, int pageSize = 10 );
 
