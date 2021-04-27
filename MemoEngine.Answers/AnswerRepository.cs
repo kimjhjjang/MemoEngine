e3a6c0fbf17815e63e1557e17a0fb6c3ebc53a0b
정훈 김 <kimjhjjang@gmail.com>
2021-04-27 21:49:25 +0900

    댓글 기능 구현 중

--- MemoEngine.Answers/AnswerRepository.cs
+++ MemoEngine.Answers/AnswerRepository.cs
@@ -1,8 +1,8 @@
 ﻿using Dapper;
 using System;
-using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace MemoEngine.Answers
--- MemoEngine/Answers/Controls/BoardCommentControl.ascx.cs
+++ MemoEngine/Answers/Controls/BoardCommentControl.ascx.cs
@@ -9,7 +9,26 @@ namespace MemoEngine.Answers.Controls
 {
     public partial class BoardCommentControl : System.Web.UI.UserControl
     {
+        private readonly IAnswerCommentRepository repository;
+
+        public BoardCommentControl()
+        {
+             repository = new AnswerCommentRepository();
+        }
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                DisplayData();
+            }
+        }
+
+        private void DisplayData()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnWriteComment_Click(object sender, EventArgs e)
         {
 
         }
